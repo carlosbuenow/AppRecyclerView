c0b0b586585d860a414e190c4edba336a0271551
carlosbuenow <carlosbueno_53@hotmail.com>
2026-05-28 21:39:04 -0300

    Criando classes no Main Activity

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -7,8 +7,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<Livros> lstLivros;
+    RecyclerView idRecLivros;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        idRecLivros = findViewById(R.id.idRecLivros);
+
+        AdapterLivros adapterLivros = new AdapterLivros(getApplicationContext(), lstLivros);
+
     }
 }
