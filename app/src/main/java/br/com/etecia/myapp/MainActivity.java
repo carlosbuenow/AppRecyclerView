436376108dce559a1ecee62b8285cbf11b96dc5d
carlosbuenow <carlosbueno_53@hotmail.com>
2026-05-28 22:09:26 -0300

    Lista de Livros com RecyclerView - GridLayout

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -7,8 +7,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,7 +31,20 @@ public class MainActivity extends AppCompatActivity {
         });
         idRecLivros = findViewById(R.id.idRecLivros);
 
+        lstLivros = new ArrayList<>();
+
+        lstLivros.add(new Livros("A Metamorfose", "Drama", R.drawable.metamorfose, 12.99));
+        lstLivros.add(new Livros("Diário de um Banana 1", "Comédia", R.drawable.diariodeumbanana, 46.46));
+        lstLivros.add(new Livros("O Pequeno Príncipe", "Ficção Científica", R.drawable.pequenoprincipe, 13.20));
+        lstLivros.add(new Livros("Cinquenta Tons de Cinza", "Romance", R.drawable.cinquentatonsdecinza, 54.45));
+        lstLivros.add(new Livros("A Guerra dos Tronos", "Ficção Científica", R.drawable.aguerradostronos, 78.11));
+        lstLivros.add(new Livros("A psicologia financeira", "Autoajuda", R.drawable.psicologiafinanceira, 29.88));
+
         AdapterLivros adapterLivros = new AdapterLivros(getApplicationContext(), lstLivros);
+
+        idRecLivros.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+
+        idRecLivros.setAdapter(adapterLivros);
 
     }
 }
